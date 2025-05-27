b0099e806e82126df08bfd079ecce16a50a20630
wlnzed <dev.thyself683@simplelogin.fr>
2025-05-27 20:14:17 +0100

    wip: frontend styles

--- my-tasks-frontend/src/components/Task.tsx
+++ my-tasks-frontend/src/components/Task.tsx
@@ -12,14 +12,24 @@ const Task: React.FC<{
   }[];
 }> = ({ title, description, isDone, subtasks }) => (
   <li className={styles.task}>
-    <h1 className={styles.taskTitle}>{title}</h1>
-    <p className={styles.taskDescription}>{description}</p>
-    <div className={styles.isDone}>{isDone ? "done" : "not done"}</div>
-    <ul className={styles.subtasks}>
-      {subtasks.map((subtask, i) => (
-        <Subtask key={i} title={subtask.title} isDone={subtask.isDone} />
-      ))}
-    </ul>
+    <main className={styles.taskMain}>
+      <div className={styles.taskMainLeft}>
+        <h1 className={styles.taskTitle}>{title}</h1>
+        <p className={styles.taskDescription}>{description}</p>
+      </div>
+      <div className={styles.taskMainRight}>
+        <div className={styles.taskIsDone}>{isDone ? "done" : "not done"}</div>
+      </div>
+    </main>
+    <footer className={styles.taskFooter}>
+      {subtasks.length > 0 && (
+        <ul className={styles.subtasks}>
+          {subtasks.map((subtask, i) => (
+            <Subtask key={i} title={subtask.title} isDone={subtask.isDone} />
+          ))}
+        </ul>
+      )}
+    </footer>
   </li>
 );
 
