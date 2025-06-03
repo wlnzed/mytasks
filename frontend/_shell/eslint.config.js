cda437e74dd174d6b15b555e721e2c029cfe10b7
wlnzed <dev.thyself683@simplelogin.fr>
2025-06-03 14:18:04 +0100

    import prettier config as flat

--- frontend/_shell/eslint.config.js
+++ frontend/_shell/eslint.config.js
@@ -3,7 +3,7 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
-import prettierConfig from "eslint-config-prettier";
+import prettierConfig from "eslint-config-prettier/flat";
 
 export default tseslint.config(
   { ignores: ["dist"] },
@@ -11,7 +11,7 @@ export default tseslint.config(
     extends: [
       js.configs.recommended,
       ...tseslint.configs.recommended,
-      prettierConfig.rules,
+      prettierConfig,
     ],
     files: ["**/*.{ts,tsx}"],
     languageOptions: {
