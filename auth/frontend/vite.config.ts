adfb9f843306c8351b913e44d4809af928521bb1
wlnzed <dev.thyself683@simplelogin.fr>
2025-06-04 11:59:35 +0100

    set https, ports, and esnext build flags on auth frontend

--- auth/frontend/vite.config.ts
+++ auth/frontend/vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), basicSsl()],
+  server: {
+    port: 5175,
+  },
+  preview: {
+    port: 5175,
+  },
+  build: {
+    target: "esnext",
+  },
+});
