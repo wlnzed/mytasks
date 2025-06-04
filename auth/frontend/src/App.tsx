5729163bbf5ba65a6b65ef756bea1d4825c2a9aa
wlnzed <dev.thyself683@simplelogin.fr>
2025-06-04 14:00:26 +0100

    sign up frontend code is exported into its own component

--- auth/frontend/src/App.tsx
+++ auth/frontend/src/App.tsx
@@ -1,70 +1,10 @@
-import React, { useState } from "react";
+import SignUp from "./components/SignUp";
 import styles from "./App.module.css";
 
-const App = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [passwordConfirmation, setPasswordConfirmation] = useState("");
-
-  const submit = (e: React.MouseEvent) => {
-    e.preventDefault();
-
-    fetch(import.meta.env.VITE_BACKEND_URL + "/sign-up", {
-      method: "post",
-      body: JSON.stringify({
-        username: username,
-        password: password,
-        passwordConfirmation: passwordConfirmation,
-      }),
-      headers: { "Content-Type": "application/json" },
-    });
-  };
-
-  return (
-    <div className={styles.app}>
-      <form className={styles.signUpForm}>
-        <label className={styles.usernameInputLabel} htmlFor="username-input">
-          Username:
-        </label>
-        <input
-          id="username-input"
-          className={styles.usernameInput}
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-
-        <label className={styles.passwordInputLabel} htmlFor="password-input">
-          Password:
-        </label>
-        <input
-          id="password-input"
-          className={styles.passwordInput}
-          type="text"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-
-        <label
-          className={styles.passwordConfirmationInputLabel}
-          htmlFor="password-confirmation-input"
-        >
-          Password Confirmation:
-        </label>
-        <input
-          id="password-confirmation-input"
-          className={styles.passwordConfirmationInput}
-          type="text"
-          value={passwordConfirmation}
-          onChange={(e) => setPasswordConfirmation(e.target.value)}
-        />
-
-        <button className={styles.submitButton} type="submit" onClick={submit}>
-          Submit
-        </button>
-      </form>
-    </div>
-  );
-};
+const App = () => (
+  <div className={styles.app}>
+    <SignUp />
+  </div>
+);
 
 export default App;
---
+++ auth/frontend/src/components/SignUp.tsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import styles from "./SignUp.module.css";
+
+const SignUp = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [passwordConfirmation, setPasswordConfirmation] = useState("");
+
+  const submit = (e: React.MouseEvent) => {
+    e.preventDefault();
+
+    fetch(import.meta.env.VITE_BACKEND_URL + "/sign-up", {
+      method: "post",
+      body: JSON.stringify({
+        username: username,
+        password: password,
+        passwordConfirmation: passwordConfirmation,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+  };
+
+  return (
+    <form className={styles.signUpForm}>
+      <label className={styles.usernameInputLabel} htmlFor="username-input">
+        Username:
+      </label>
+      <input
+        id="username-input"
+        className={styles.usernameInput}
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+
+      <label className={styles.passwordInputLabel} htmlFor="password-input">
+        Password:
+      </label>
+      <input
+        id="password-input"
+        className={styles.passwordInput}
+        type="text"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+
+      <label
+        className={styles.passwordConfirmationInputLabel}
+        htmlFor="password-confirmation-input"
+      >
+        Password Confirmation:
+      </label>
+      <input
+        id="password-confirmation-input"
+        className={styles.passwordConfirmationInput}
+        type="text"
+        value={passwordConfirmation}
+        onChange={(e) => setPasswordConfirmation(e.target.value)}
+      />
+
+      <button className={styles.submitButton} type="submit" onClick={submit}>
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default SignUp;
