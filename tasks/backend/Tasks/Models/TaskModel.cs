306671872e897445a93671a06af11d00350cd4f4
wlnzed <dev.thyself683@simplelogin.fr>
2025-06-05 16:41:44 +0100

    dynamodb attributes are removed from task model fields as redundant

--- tasks/backend/Tasks/Models/TaskModel.cs
+++ tasks/backend/Tasks/Models/TaskModel.cs
@@ -5,30 +5,22 @@ namespace Tasks.Models;
 [DynamoDBTable("mytasks-tasks-dev")]
 public class TaskModel
 {
-    [DynamoDBHashKey("OwnerEmail")]
     public string? OwnerEmail { get; set; }
 
-    [DynamoDBRangeKey("Id")]
     public string? Id { get; set; }
 
-    [DynamoDBProperty("Title")]
     public string? Title { get; set; }
 
-    [DynamoDBProperty("Description")]
     public string? Description { get; set; }
 
-    [DynamoDBProperty("IsDone")]
     public bool? IsDone { get; set; }
 
-    [DynamoDBProperty("Subtasks")]
     public Subtask[]? Subtasks { get; set; }
 }
 
 public class Subtask
 {
-    [DynamoDBProperty("Title")]
     public string? Title { get; set; }
 
-    [DynamoDBProperty("IsDone")]
     public bool? IsDone { get; set; }
 }
