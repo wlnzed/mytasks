f57ff9d4ea037bc02d9b7db86d1f21882ba1ac9d
wlnzed <dev.thyself683@simplelogin.fr>
2025-06-06 15:46:30 +0100

    add unit tests for task component

---
+++ tasks/frontend/src/components/Task.test.tsx
@@ -0,0 +1,45 @@
+import { afterEach, test } from "vitest";
+import { cleanup, render, screen } from "@testing-library/react";
+import Task from "./Task";
+
+afterEach(cleanup);
+
+test("renders task data (not done)", () => {
+  const title = "Task #1";
+  const description = "Do something";
+  const isDone = false;
+
+  render(
+    <Task
+      id=""
+      title={title}
+      description={description}
+      isDone={isDone}
+      subtasks={[]}
+    />,
+  );
+
+  screen.getByText(title);
+  screen.getByText(description);
+  screen.getByText("not done");
+});
+
+test("renders task data (done)", () => {
+  const title = "Task #2";
+  const description = "Do something else";
+  const isDone = true;
+
+  render(
+    <Task
+      id=""
+      title={title}
+      description={description}
+      isDone={isDone}
+      subtasks={[]}
+    />,
+  );
+
+  screen.getByText(title);
+  screen.getByText(description);
+  screen.getByText("done");
+});
