c680683e8b3c08d4862f8b02661ad4e217d5bc7e
wlnzed <dev.thyself683@simplelogin.fr>
2025-06-06 21:11:52 +0100

    add "navigates to home" unit test to auth mfe

---
+++ auth/frontend/src/__tests__/unit/App.test.tsx
@@ -0,0 +1,29 @@
+import { afterEach, expect, test } from "vitest";
+import { cleanup, render } from "@testing-library/react";
+import Cookies from "js-cookie";
+import App from "../../App";
+
+afterEach(cleanup);
+
+test("navigates to sign in when no user email cookie", async () => {
+  const expectedPath = "/sign-in";
+  Object.defineProperty(window, "location", {
+    value: { origin: "https://localhost:1234", pathname: "/" },
+  });
+
+  render(<App />);
+
+  expect(window.location.pathname).toBe(expectedPath);
+});
+
+test("navigates to home when user email cookie is present", async () => {
+  const expectedPath = "/home";
+  Object.defineProperty(window, "location", {
+    value: { origin: "https://localhost:1234", pathname: "/" },
+  });
+  Cookies.set("user-email", "user@email.com");
+
+  render(<App />);
+
+  expect(window.location.pathname).toBe(expectedPath);
+});
