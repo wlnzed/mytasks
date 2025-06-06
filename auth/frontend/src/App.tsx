d48836f5143df6cb9a91a837e1b5ff66d1284663
wlnzed <dev.thyself683@simplelogin.fr>
2025-06-06 12:48:34 +0100

    check for user email cookie and navigate accordingly in auth mfe

--- auth/frontend/src/App.tsx
+++ auth/frontend/src/App.tsx
@@ -1,15 +1,29 @@
+import { useEffect } from "react";
 import SignUp from "./components/SignUp";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import styles from "./App.module.css";
+import Cookies from "js-cookie";
 
 const App = () => {
-  // window.location.pathname = "/sign-in";
+  useEffect(() => {
+    if (
+      window.location.pathname !== "/sign-in" &&
+      window.location.pathname !== "/sign-up"
+    ) {
+      if (Cookies.get("user-email") === undefined) {
+        window.location.pathname = "/sign-in";
+      } else if (window.location.pathname !== "/home") {
+        window.location.pathname = "/home";
+      }
+    }
+  }, []);
 
   return (
     <div className={styles.app}>
       <Router>
         <Routes>
-          <Route path="/" element={<>TODO: SIGNED IN VIEW</>} />
+          <Route path="/" element={<>TODO: SPINNER</>} />
+          <Route path="/home" element={<>TODO: SIGNED IN VIEW</>} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/sign-in" element={<>TODO: SIGN IN VIEW</>} />
         </Routes>
