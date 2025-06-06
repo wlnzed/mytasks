0bf7f0c0d473260629c0fdf9999f78585fa73866
wlnzed <dev.thyself683@simplelogin.fr>
2025-06-06 16:00:14 +0100

    move fetch/render tasks logic back to App.tsx

--- tasks/frontend/src/App.tsx
+++ tasks/frontend/src/App.tsx
@@ -1,5 +1,38 @@
-import Tasks from "./components/Tasks";
+import { useEffect, useState } from "react";
+import Task from "./components/Task";
+import tasksApi from "./api/tasks";
+import type { ITask } from "./types/ITask";
+import styles from "./App.module.css";
 
-const App = () => <Tasks />;
+const App = () => {
+  const [tasks, setTasks] = useState<ITask[]>([]);
+
+  useEffect(() => {
+    tasksApi
+      .get()
+      .then((res) => {
+        res
+          .json()
+          .then(setTasks)
+          .catch((err) => console.log("error while parsing tasks: ", err));
+      })
+      .catch((err) => console.log("error while fetching tasks: " + err));
+  }, []);
+
+  return (
+    <ul className={styles.tasks}>
+      {tasks.map((task) => (
+        <Task
+          key={task.id}
+          id={task.id}
+          title={task.title}
+          description={task.description}
+          isDone={task.isDone}
+          subtasks={task.subtasks}
+        />
+      ))}
+    </ul>
+  );
+};
 
 export default App;
