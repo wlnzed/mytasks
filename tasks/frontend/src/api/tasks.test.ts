24f69b4db203e5f5029e167f91dbb71724f967dd
wlnzed <dev.thyself683@simplelogin.fr>
2025-06-06 18:06:39 +0100

    add return response unit test to tasks api

--- tasks/frontend/src/api/tasks.test.ts
+++ tasks/frontend/src/api/tasks.test.ts
@@ -11,3 +11,14 @@ test("makes get request with credentials to the tasks endpoint", () => {
     { credentials: "include" },
   );
 });
+
+test("returns response as-is from the fetch call", async () => {
+  const expectedResponse = "some response";
+  const spyFetch = vi.fn();
+  spyFetch.mockResolvedValue(Promise.resolve(expectedResponse));
+  global.fetch = spyFetch;
+
+  const actualResponse = await tasksApi.get();
+
+  expect(actualResponse).toEqual(expectedResponse);
+});
