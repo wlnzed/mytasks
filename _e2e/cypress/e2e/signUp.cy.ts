04262630803767ae6cbc624f4c433f337e740ceb
wlnzed <dev.thyself683@simplelogin.fr>
2025-06-08 15:58:42 +0100

    add "submit sign up" e2e test

--- _e2e/cypress/e2e/signUp.cy.ts
+++ _e2e/cypress/e2e/signUp.cy.ts
@@ -1,11 +1,25 @@
 describe("sign up", () => {
-  it("navigate to sign in", () => {
-    cy.visit(Cypress.env("appUrl") + "/sign-up");
+  beforeEach(() => cy.visit(Cypress.env("appUrl") + "/sign-up"));
 
+  it("navigates to sign in view on sign in button click", () => {
     const signInButton = cy.findByText("Sign In");
-
     signInButton.click();
-
     cy.findByText("TODO: SIGN IN VIEW");
+  });
+
+  it("signs up the new user on submit", () => {
+    const usernameInput = cy.findByLabelText("Username:");
+    const passwordInput = cy.findByLabelText("Password:");
+    const passwordConfirmationInput = cy.findByLabelText(
+      "Password Confirmation:",
+    );
+    const submitButton = cy.findByText("Submit");
+
+    usernameInput.type("foo");
+    passwordInput.type("bar");
+    passwordConfirmationInput.type("baz");
+    submitButton.click();
+
+    // TODO: test for sign up behavior once implemented
   });
 });
