3012d434a134074b811309eadb350ac24c7df3ae
wlnzed <dev.thyself683@simplelogin.fr>
2025-06-09 00:29:54 +0100

    define dynamodb in terraform

---
+++ _isx/dynamodb.tf
@@ -0,0 +1,16 @@
+resource "aws_dynamodb_table" "prod-mytasks-tasks" {
+  name         = "prod-mytasks-tasks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "OwnerEmail"
+  range_key    = "Id"
+
+  attribute {
+    name = "OwnerEmail"
+    type = "S"
+  }
+
+  attribute {
+    name = "Id"
+    type = "S"
+  }
+}
