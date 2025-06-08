fbf028660463f0d7a4e44932bb48847302f68595
wlnzed <dev.thyself683@simplelogin.fr>
2025-06-08 14:42:07 +0100

    add "navigate to sign in" end to end test to sign up

--- _e2e/cypress/e2e/signUp.cy.ts
+++ _e2e/cypress/e2e/signUp.cy.ts
@@ -1,6 +1,11 @@
-describe("template spec", () => {
-  it("passes", () => {
-    cy.visit(Cypress.env("appUrl"));
+describe("sign up", () => {
+  it("navigate to sign in", () => {
+    cy.visit(Cypress.env("appUrl") + "/sign-up");
+
+    const signInButton = cy.findByText("Sign In");
+
+    signInButton.click();
+
+    cy.findByText("TODO: SIGN IN VIEW");
   });
 });
-
