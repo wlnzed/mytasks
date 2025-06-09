1a07071939031aa3b0818c6f26622f0d2b1f814e
wlnzed <dev.thyself683@simplelogin.fr>
2025-06-09 01:16:51 +0100

    define cognito user pool in terraform

---
+++ _isx/cognito.tf
@@ -0,0 +1,20 @@
+resource "aws_cognito_user_pool" "mytasks" {
+  name                = "mytasks"
+  username_attributes = ["email"]
+
+  username_configuration {
+    case_sensitive = false
+  }
+
+  schema {
+    name                = "email"
+    attribute_data_type = "String"
+    mutable             = true
+    required            = true
+
+    string_attribute_constraints {
+      min_length = 5
+      max_length = 2048
+    }
+  }
+}
