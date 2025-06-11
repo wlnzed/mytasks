0a75d699650da7127894a868bfbeaaf8e7fbea17
wlnzed <gitlab.crafty248@aleeas.com>
2025-06-11 18:50:20 +0100

    ci: force s3 destroys

--- _isx/s3.tf
+++ _isx/s3.tf
@@ -1,11 +1,14 @@
 resource "aws_s3_bucket" "mytasks_shell" {
-  bucket = "${var.ENV}-mytasks-shell"
+  bucket        = "${var.ENV}-mytasks-shell"
+  force_destroy = true
 }
 
 resource "aws_s3_bucket" "mytasks_auth" {
-  bucket = "${var.ENV}-mytasks-auth"
+  bucket        = "${var.ENV}-mytasks-auth"
+  force_destroy = true
 }
 
 resource "aws_s3_bucket" "mytasks_tasks" {
-  bucket = "${var.ENV}-mytasks-tasks"
+  bucket        = "${var.ENV}-mytasks-tasks"
+  force_destroy = true
 }
